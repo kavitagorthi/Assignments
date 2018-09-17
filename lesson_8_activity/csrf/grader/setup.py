faa643f33f8cfd4731afdcefb175ed62ec5b282c
Gorthi Kavita <gorthi.kavita@gmail.com>
2018-09-17 12:00:37 -0700

    Commit message.

--- lesson_8_activity/csrf/grader/setup.py
+++
@@ -1,4 +0,0 @@
-from model import db, Grade 
-
-db.connect()
-db.create_tables([Grade])
--- lesson_8_activity/myblog_rssfeed/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from myblog.models import Post
-from myblog.models import Category
-
-admin.site.register(Post)
-admin.site.register(Category)
-
-# Register your models here.
